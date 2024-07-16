df3a1207b4da2dbb357fec09a6c96fca28d984f0
krishn110790 <krishna.raghuwanshi@billennium.com>
2024-07-17 03:24:03 +0530

    Update streamlit_app.py
    using df for fruityvice data

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,8 @@ from snowflake.snowpark.functions import col
 import requests
 
 #Import fruityvice_response data
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
 
+#st.text(fruityvice_response.json())
 
 
 # Write directly to the app
@@ -42,7 +41,9 @@ if ingredients_list:
     ingredients_string=''
     for fruit_chosen in ingredients_list:
         ingredients_string+= fruit_chosen + ' '
-    
+        
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string +"""' , '"""+ name_on_order + """')"""
